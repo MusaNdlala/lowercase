b09bc8c2924131b4104f8acd238da4264698b8bb
Musa <Musa@DESKTOP-N08JB8A>
2021-09-10 17:29:13 +0200

    Add project files.

---
+++ lowercase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lowercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Char c = Convert.ToChar(Console.ReadLine());
+            tolower(c);
+            Console.Read();
+        }
+        static void tolower(char c)
+        {
+            c = Convert.ToChar(c + 32);
+            Console.WriteLine(c);
+        }
+    }
+}
